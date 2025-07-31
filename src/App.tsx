4fcd0c435d94e0b6114cea3b1b378193067f21c2
Kheil <cunanankheilanthony@gmail.com>
2025-07-31 11:56:05 +0800

    project initialization

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+// import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
+      <h1 className="text-6xl font-extrabold text-white shadow-lg p-6 rounded-2xl">
+        Tailwind Works 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
